ea1eeac3218b622c14809fbdd0802a9a3e0263c3
Ananya Neogi <ananya.neogi@fs-students.de>
2019-05-27 23:37:57 +0200

    changes made in dataframes merge + data description file added

--- Time_series_EDA_Feature_Engineering.py
+++ Time_series_EDA_Feature_Engineering.py
@@ -35,11 +35,50 @@ print("Solid Item dataframe columns :",df1.columns)
 
 print("Content dataframe shape :", df2.shape)
 print("Content dataframe columns :",df2.columns)
+df2.describe()
+pd.set_option('display.max_rows', 1000)
+
+manufacturers_to_drop = ['Manufacturer_1','Manufacturer_2','Manufacturer_9','Manufacturer_4','Manufacturer_5',
+                        'Manufacturer_6','Manufacturer_7','Manufacturer_8','Manufacturer_10','Manufacturer_11',
+                        'Manufacturer_12','Manufacturer_13','Manufacturer_15','Manufacturer_16','Manufacturer_17',
+                        'Manufacturer_18','Manufacturer_19','Manufacturer_20','Manufacturer_21','Manufacturer_22',
+                        'Manufacturer_23','Manufacturer_24','Manufacturer_25','Manufacturer_26','Manufacturer_27',
+                        'Manufacturer_28','Manufacturer_29','Manufacturer_30','Manufacturer_31','Manufacturer_32',
+                        'Manufacturer_34','Manufacturer_35','Manufacturer_36','Manufacturer_37','Manufacturer_38',
+                        'Manufacturer_39','Manufacturer_40','Manufacturer_41','Manufacturer_42','Manufacturer_43',
+                        'Manufacturer_44','Manufacturer_45','Manufacturer_46','Manufacturer_47','Manufacturer_48',
+                        'Manufacturer_49','Manufacturer_50','Manufacturer_51','Manufacturer_52','Manufacturer_53',
+                        'Manufacturer_54','Manufacturer_55','Manufacturer_56','Manufacturer_57','Manufacturer_58',
+                        'Manufacturer_59','Manufacturer_60','Manufacturer_61','Manufacturer_62','Manufacturer_63',
+                        'Manufacturer_64','Manufacturer_65','Manufacturer_66','Manufacturer_67','Manufacturer_68',
+                        'Manufacturer_69','Manufacturer_70','Manufacturer_71','Manufacturer_72','Manufacturer_73',
+                        'Manufacturer_74']
+df2 = df2.drop(manufacturers_to_drop, axis = 1)
+df2.shape
+
+count_rows = df2.apply(lambda x: True if x['Manufacturer_0'] or x['Manufacturer_3'] or x['Manufacturer_14'] or x['Manufacturer_33'] == 1 else False , axis=1)
+num_rows = len(count_rows[count_rows == True].index)
+print('Number of Rows where value is 1 for the manufacturers are: ', num_rows)
+
+df2 = df2[df2['Manufacturer_0'] | df2['Manufacturer_3'] | df2['Manufacturer_14'] | df2['Manufacturer_33'] == 1] 
+df2.shape
+
 
 #%% Display the two original dataframes and the merged dataframe
 
 df3 = pd.merge(df1, df2, left_on="product_sid", right_on="ProductId")
-df3.head()
+print(df3.head())
 print(df3.shape)
 pd.set_option('display.max_rows', 1000)
 print(df3.isna().sum())
+
+print(df3.describe())
+
+#%% Dimensionality reduction
+
+from sklearn.preprocessing import minmax_scale
+from sklearn.model_selection import train_test_split
+from keras.layers import Input, Dense
+from keras.models import Model
+
+)
